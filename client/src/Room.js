32d3ca2e7209678d2d78f84a6709a07470d45245
junheeLee96 <dlwnsgml203@naver.com>
2023-10-23 01:46:03 +0900

    peerjs start

--- client/src/Room.js
+++ client/src/Room.js
@@ -1,13 +1,22 @@
 import React, { useEffect, useRef } from "react";
 import { io } from "socket.io-client";
+import Peer from "peerjs";
 
 const Room = () => {
   const socketRef = useRef(null);
+  const peerRef = useRef(null);
 
   useEffect(() => {
     socketRef.current = io.connect("http://localhost:9000");
 
     socketRef.current.emit("join-room");
+
+    peerRef.current = new Peer();
+
+    peerRef.current.on("open", (id) => {
+      //data connection
+      console.log(id);
+    });
   }, []);
 
   return <div></div>;
