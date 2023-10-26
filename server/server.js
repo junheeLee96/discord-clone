6178e034950c8c0f81b156cd204552895e9478bd
junheeLee96 <dlwnsgml203@naver.com>
2023-10-26 22:38:04 +0900

    chat stream

--- server/server.js
+++ server/server.js
@@ -18,8 +18,32 @@ const PORT = 9000;
 io.on("connection", (socket) => {
   console.log("connection!!");
 
-  socket.on("join-room", () => {
-    console.log("join-rooom!!");
+  socket.on("join-room", (roomid, id, nickname) => {
+    socket.join(roomid);
+    console.log(id);
+    //new user join to the room
+
+    //sender를 제외한 방의 모든 사람에게 메세지를 날림
+    socket.broadcast.to(roomid).emit("user-connected", id, nickname);
+    socket.on("disconnect", () => {
+      //유저의 소켓연결이 끊어졌을때
+
+      socket.leave(roomid);
+      //방에서 내보내고
+
+      //모두에게 알린다
+      socket.to(roomid).emit("user-disconnected", id);
+    });
+
+    socket.on("streaming-start", (roomid, id) => {
+      //sender 빼고 모든 룸안의 유저들에게 메세지를 날림
+      socket.broadcast.to(roomid).emit("other-user-streaming-start", id);
+    });
+
+    socket.on("message-send", (ms, id, roomid) => {
+      //sender를 포함한 룸 유저 전체에게 메세지
+      io.in(roomid).emit("receive-message", ms, id);
+    });
   });
 });
 
