4482ae26327fbaa64fd83c4291a9140be989b7bd
junheeLee96 <dlwnsgml203@naver.com>
2023-11-28 00:46:08 +0900

    일단 됏는데... 문제가 잇긴하네

--- client/src/room/pure/PureRTC.js
+++ client/src/room/pure/PureRTC.js
@@ -5,8 +5,8 @@ import { io } from "socket.io-client";
 export const Video = ({ stream }) => {
   const ref = useRef();
   useEffect(() => {
-    console.log(stream);
-    ref.current.srcObject = stream;
+    // console.log(stream);
+    ref.current.srcObject = stream.stream;
   }, [stream]);
   return (
     <div>
@@ -20,13 +20,14 @@ const PureRTC = () => {
   const peerRef = useRef(null);
   const socketRef = useRef(null);
   const otherUserRef = useRef(null);
-  const [users, setUsers] = useState([]);
-  const [peers, setPeers] = useState([]);
+  const idRef = useRef(null);
+  const [users, setUsers] = useState({});
+  const [peers, setPeers] = useState({});
 
   const roomId = "123123";
 
   useEffect(() => {
-    console.log(users);
+    // console.log(users);
   }, [users]);
 
   useEffect(() => {
@@ -54,13 +55,16 @@ const PureRTC = () => {
 
     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
       const id = socket.id;
+      idRef.current = id;
       console.log("my id = ", id);
       const audio = stream.getAudioTracks()[0];
       audio.enabled = false;
       myRef.current.srcObject = stream;
       peerRef.current.addStream(stream);
+      let answers = [];
+      let ices = [];
 
-      function addEvent(peer, sender) {
+      async function addEvent(peer, sender) {
         peer.addEventListener("icecandidate", (data) => {
           console.log("ice!!addeventlistener");
           socket.emit("ice", data.candidate, sender);
@@ -70,50 +74,43 @@ const PureRTC = () => {
           // console.log(event.streams[0]);
           console.log("track!!");
           const stream = event.streams[0];
-          if (!stream) return;
-          console.log(stream);
-          setUsers((p) => [...p, stream]);
-          console.log(stream);
-          otherUserRef.current.srcObject = stream;
-          stream.getTracks().forEach((track) => {
-            track.addEventListener("ended", () => {
-              console.log("end!!");
-            });
+          setUsers((p) => {
+            console.log(p);
+            return {
+              ...p,
+              [sender]: {
+                stream,
+                peer,
+              },
+            };
           });
-          // otherUserRef.current.srcObject = data.streams[0];
+          if (!stream) return;
         });
+
+        // peer.addEventListener("onconnectionstatechange", () => {
+        //   console.log("Connection State:", peer.connectionState);
+
+        //   if (peer.connectionState === "disconnected") {
+        //     // 피어의 연결이 끊어졌을 때 처리할 로직을 여기에 추가
+        //     console.log("Peer connection disconnected!");
+        //     setUsers((p) => {
+        //       return delete p[sender];
+        //     });
+        //   }
+        // });
       }
-      socket.emit("join-room", roomId, id);
-
-      socket.on("offs", async (offer, sender) => {
-        console.log("offs = ", offer);
-        const peer = new RTCPeerConnection({
-          iceServers: [
-            {
-              urls: "stun:stun.l.google.com:19302",
-            },
-          ],
-        });
-        addEvent(peer, sender);
-
-        // stream.getTracks().forEach((track) => {
-        //   peer.addTrack(track, stream);
-        // });
-        peer.addStream(stream);
-        await peer.setRemoteDescription(offer);
-
-        const answer = await peer.createAnswer();
-        await peer.setLocalDescription(answer);
-        socket.emit("answers", answer, sender);
-
-        socket.on("icezz", (ice) => {
-          peer.addIceCandidate(ice);
-        });
+      let current_peer;
+      const peer = new RTCPeerConnection({
+        iceServers: [
+          {
+            urls: "stun:stun.l.google.com:19302",
+          },
+        ],
       });
-      //
-      //
 
-      socket.on("new_user_connected", async (receiver) => {
+      console.log(peer);
+      async function createOffer(user) {
+        console.log(user);
         const peer = new RTCPeerConnection({
           iceServers: [
             {
@@ -122,26 +119,190 @@ const PureRTC = () => {
           ],
         });
 
-        // stream.getTracks().forEach((track) => {
-        //   peer.addTrack(track, stream);
-        // });
+        console.log(peer);
         peer.addStream(stream);
-
+        addEvent(peer, user);
         const offer = await peer.createOffer();
-        console.log(offer);
+        peer.setLocalDescription(offer);
+        console.log("ananana");
+        socket.emit("send_offer", offer, user, id);
 
-        await peer.setLocalDescription(offer);
-
-        socket.emit("offs", offer, receiver, id);
-
-        socket.on("answers", async (answer) => {
+        socket.on("send_answer", async (answer) => {
+          console.log("answer = ", answer);
+          if (answers.includes(JSON.stringify(answer))) {
+            return;
+          }
+          console.log(peer);
+          answers.push(JSON.stringify(answer));
           await peer.setRemoteDescription(answer);
         });
-        addEvent(peer, receiver);
-        socket.on("icezz", async (ice) => {
+        socket.on("icezz", (ice) => {
+          if (ices.includes(JSON.stringify(ice))) {
+            return;
+          }
+          console.log(peer);
+          ices.push(JSON.stringify(ice));
+          peer.addIceCandidate(ice);
+        });
+      }
+
+      socket.emit("join-room", roomId, id);
+
+      socket.on("origin_users", (users) => {
+        console.log(users);
+
+        users.forEach(async (user) => {
+          if (user !== id) {
+            // createOffer(user);
+            const peer = new RTCPeerConnection({
+              iceServers: [
+                {
+                  urls: "stun:stun.l.google.com:19302",
+                },
+              ],
+            });
+
+            console.log(peer);
+            peer.addStream(stream);
+            addEvent(peer, user);
+            const offer = await peer.createOffer();
+            peer.setLocalDescription(offer);
+            console.log("ananana");
+            socket.emit("send_offer", offer, user, id);
+
+            socket.on(`send_answer${user}`, async (answer) => {
+              console.log("answer = ", answer);
+              // if (answers.includes(JSON.stringify(answer))) {
+              //   return;
+              // }
+              console.log(peer);
+              answers.push(JSON.stringify(answer));
+              await peer.setRemoteDescription(answer);
+            });
+            socket.on(`icezz${user}`, (ice) => {
+              // if (ices.includes(JSON.stringify(ice))) {
+              //   return;
+              // }
+              console.log(peer);
+              ices.push(JSON.stringify(ice));
+              peer.addIceCandidate(ice);
+            });
+          }
+        });
+      });
+
+      socket.on("send_offer", async (offer, sender) => {
+        console.log("sned offer");
+        const peer = new RTCPeerConnection({
+          iceServers: [
+            {
+              urls: "stun:stun.l.google.com:19302",
+            },
+          ],
+        });
+        peer.addStream(stream);
+        // addEvent(peer, sender);
+        peer.addEventListener("icecandidate", (data) => {
+          console.log("ice!!addeventlistener");
+          socket.emit("ice", data.candidate, sender, id);
+        });
+
+        peer.addEventListener("track", (event) => {
+          // console.log(event.streams[0]);
+          console.log("track!!");
+          const stream = event.streams[0];
+          setUsers((p) => {
+            console.log(p);
+            return {
+              ...p,
+              [sender]: {
+                stream,
+                peer,
+              },
+            };
+          });
+          if (!stream) return;
+        });
+        await peer.setRemoteDescription(offer);
+        const answer = await peer.createAnswer();
+        console.log("answer = ", answer);
+        peer.setLocalDescription(answer);
+        socket.emit("send_answer", answer, sender, id);
+        socket.on("icezz", (ice) => {
+          console.log(ice);
           peer.addIceCandidate(ice);
         });
       });
+
+      //
+      //
+      //
+      //
+      //
+      //
+      //
+      //
+      //
+      //
+
+      // socket.on("offs", async (offer, sender) => {
+      //   return;
+      //   console.log("offs = ", offer);
+      //   const peer = new RTCPeerConnection({
+      //     iceServers: [
+      //       {
+      //         urls: "stun:stun.l.google.com:19302",
+      //       },
+      //     ],
+      //   });
+      //   addEvent(peer, sender);
+
+      //   // stream.getTracks().forEach((track) => {
+      //   //   peer.addTrack(track, stream);
+      //   // });
+      //   peer.addStream(stream);
+      //   await peer.setRemoteDescription(offer);
+
+      //   const answer = await peer.createAnswer();
+      //   await peer.setLocalDescription(answer);
+      //   socket.emit("answers", answer, sender);
+
+      //   socket.on("icezz", (ice) => {
+      //     peer.addIceCandidate(ice);
+      //   });
+      // });
+      // //
+      // //
+
+      // socket.on("new_user_connected", async (receiver) => {
+      //   const peer = new RTCPeerConnection({
+      //     iceServers: [
+      //       {
+      //         urls: "stun:stun.l.google.com:19302",
+      //       },
+      //     ],
+      //   });
+
+      //   // stream.getTracks().forEach((track) => {
+      //   //   peer.addTrack(track, stream);
+      //   // });
+      //   peer.addStream(stream);
+
+      //   const offer = await peer.createOffer();
+      //   console.log(offer);
+
+      //   await peer.setLocalDescription(offer);
+
+      //   socket.emit("offs", offer, receiver, id);
+
+      //   socket.on("answers", async (answer) => {
+      //     await peer.setRemoteDescription(answer);
+      //   });
+      //   addEvent(peer, receiver);
+      //   socket.on("icezz", async (ice) => {
+      //     peer.addIceCandidate(ice);
+      //   });
+      // });
 
       // socket.on("new_user_conn", async (new_user) => {
       //   console.log("new_user = ", new_user);
@@ -192,48 +353,55 @@ const PureRTC = () => {
       //   });
       // });
 
-      socket.on("welcom", async () => {
-        const offer = await peerRef.current.createOffer();
-        // console.log("offer = ", offer);
-        peerRef.current.setLocalDescription(offer);
+      // socket.on("welcom", async () => {
+      //   const offer = await peerRef.current.createOffer();
+      //   // console.log("offer = ", offer);
+      //   peerRef.current.setLocalDescription(offer);
 
-        socket.emit("offer", offer, roomId, id);
-      });
+      //   socket.emit("offer", offer, roomId, id);
+      // });
 
-      socket.on("offer", async (offer, id) => {
-        peerRef.current.setRemoteDescription(offer);
-        const answer = await peerRef.current.createAnswer();
-        await peerRef.current.setLocalDescription(answer);
-        socket.emit("answer", answer, roomId);
-      });
+      // socket.on("offer", async (offer, id) => {
+      //   peerRef.current.setRemoteDescription(offer);
+      //   const answer = await peerRef.current.createAnswer();
+      //   await peerRef.current.setLocalDescription(answer);
+      //   socket.emit("answer", answer, roomId);
+      // });
 
-      socket.on("ice", (ice) => {
-        peerRef.current.addIceCandidate(ice);
-      });
+      // socket.on("ice", (ice) => {
+      //   peerRef.current.addIceCandidate(ice);
+      // });
 
-      socket.on("answer", (answer) => {
-        // peerRef.current.setLocalDescription(answer);
-        peerRef.current.setRemoteDescription(answer);
-      });
+      // socket.on("answer", (answer) => {
+      //   // peerRef.current.setLocalDescription(answer);
+      //   peerRef.current.setRemoteDescription(answer);
+      // });
 
-      peerRef.current.addEventListener("icecandidate", (data) => {
-        // console.log("ice data = ", data);
-        console.log("izzz");
-        socket.emit("ice", data.candidate, roomId);
-      });
+      // peerRef.current.addEventListener("icecandidate", (data) => {
+      //   // console.log("ice data = ", data);
+      //   console.log("izzz");
+      //   socket.emit("ice", data.candidate, roomId);
+      // });
 
-      peerRef.current.addEventListener("track", (event) => {
-        const stream = event.streams[0] || new MediaStream([event.track]);
-        console.log(peerRef.current);
-        otherUserRef.current.srcObject = stream;
-        stream.getTracks().forEach((track) => {
-          track.addEventListener("ended", () => {});
-        });
-        // otherUserRef.current.srcObject = data.streams[0];
-      });
+      // peerRef.current.addEventListener("track", (event) => {
+      //   const stream = event.streams[0] || new MediaStream([event.track]);
+      //   console.log(peerRef.current);
+      //   otherUserRef.current.srcObject = stream;
+      //   stream.getTracks().forEach((track) => {
+      //     track.addEventListener("ended", () => {});
+      //   });
+      //   // otherUserRef.current.srcObject = data.streams[0];
+      // });
     });
     // .catch((error) => {
     //   console.error("Error accessing media devices.", error);
+    // });
+
+    // socket.on("disconn", (dis) => {
+    //   console.log(dis);
+    //   setUsers((p) => {
+    //     return delete p[dis];
+    //   });
     // });
   }, []);
 
@@ -242,9 +410,9 @@ const PureRTC = () => {
       <video ref={myRef} autoPlay />
       {/* {o && <video srcObject={o} autoPlay />} */}
       <video ref={otherUserRef} autoPlay />
-      {/* {users.map((st) => (
-        <Video stream={st} />
-      ))} */}
+      {Object.keys(users).map((key, idx) => (
+        <Video stream={users[key]} key={idx} />
+      ))}
     </div>
   );
 };
--- server/newFile.js
+++ server/newFile.js
@@ -1,95 +1,4 @@
-// const express = require("express");
-// const app = express();
-
-// const cors = require("cors");
-
-// const server = require("http").createServer();
-// const io = require("socket.io")(server, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST"],
-//   },
-// });
-// const PORT = 9000;
-// io.on("connection", (socket) => {
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   console.log("connection!!");
-//   console.log(socket);
-//   socket.on("join-room", (roomid, id, nickname) => {
-//     console.log("join room");
-//     socket.join(roomid);
-//     // console.log("new_user,id = ", id);
-//     //new user join to the room
-
-//     //sender를 제외한 방의 모든 사람에게 메세지를 날림
-//     socket.broadcast.to(roomid).emit("user-connected", id, nickname);
-//     socket.on("disconnect", () => {
-//       //유저의 소켓연결이 끊어졌을때
-
-//       socket.leave(roomid);
-//       //방에서 내보내고
-
-//       //모두에게 알린다
-//       socket.to(roomid).emit("user-disconnected", id);
-//     });
-
-//     socket.on("streaming-start", (roomid, id) => {
-//       //sender 빼고 모든 룸안의 유저들에게 메세지를 날림
-//       socket.broadcast.to(roomid).emit("streamer-start", id);
-//       socket.on("streaming-disconnect", (id) => {
-//         console.log("streaming stop");
-//         socket.broadcast.to(roomid).emit("streaming-disconnect", id);
-//       });
-//     });
-
-//     socket.on("message-send", (ms, id, roomid) => {
-//       //sender를 포함한 룸 유저 전체에게 메세지
-//       io.in(roomid).emit("receive-message", ms, id);
-//     });
-
-//     socket.on("zz", () => {
-//       console.log("zzzz");
-//     });
-//   });
-// });
-
-// server.listen(PORT, () => console.log("server is running on ", PORT));
-
-const express = require("express");
-const app = express();
-const http = require("http");
-const { Server } = require("socket.io");
-const server = http.createServer(app);
-
-// cors 설정을 하지 않으면 오류가 생기게 됩니다. 설정해 줍니다.
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["my-custom-header"],
-    credentials: true,
-  },
-});
-
-const PORT = process.env.PORT || 8080;
-
-// 어떤 방에 어떤 유저가 들어있는지
-let users = {};
-// socket.id기준으로 어떤 방에 들어있는지
-let socketRoom = {};
-
-// 방의 최대 인원수
-const MAXIMUM = 2;
+const { io, users, MAXIMUM, socketRoom } = require("./server");
 
 io.on("connection", (socket) => {
   // socket.on("zz", (roomid) => {
@@ -97,7 +6,6 @@ io.on("connection", (socket) => {
   //   console.log("zzz");
   //   socket.broadcast.to(roomid).emit("zzzzzz");
   // });
-
   socket.on("join-room", (roomid, id) => {
     console.log("join", id);
     socket.join(roomid);
@@ -113,26 +21,8 @@ io.on("connection", (socket) => {
   });
 
   socket.on("send_offer", (offer, user, sender) => {
-    console.log("send_offer");
     io.to(user).emit("send_offer", offer, sender);
   });
-
-  socket.on("send_answer", (answer, sender, my) => {
-    io.to(sender).emit(`send_answer${my}`, answer);
-  });
-
-  socket.on("ice", (ice, sender, my) => {
-    console.log("ice!!");
-    io.to(sender).emit(`icezz${my}`, ice);
-    // socket.to(roomid).emit("ice", ice);
-  });
-  //
-  //
-  //
-  //
-  //
-  //
-  //
 
   socket.on("offs", (offer, receiver, sender) => {
     console.log("offs");
@@ -153,7 +43,6 @@ io.on("connection", (socket) => {
   //
   //
   //
-
   socket.on("off", (offer, sender, new_user) => {
     io.to(new_user).emit("off", offer, sender);
   });
@@ -164,16 +53,13 @@ io.on("connection", (socket) => {
 
   // socket.on("send_offer_to_new_user", (offer, new_userid) => {
   //   console.log(new_userid);
-
   //   io.to(new_userid).emit("origin_users_offer", offer);
   // });
-
   //
   //
   //
   //
   //
-
   socket.on("offer", (offer, roomid, id) => {
     socket.to(roomid).emit("offer", offer, id);
     console.log("offer = ", "rooomid = ", roomid);
@@ -185,13 +71,12 @@ io.on("connection", (socket) => {
   });
 
   // socket.on("answer", answer);
-  // socket.on("ice", (ice, sender) => {
-  //   console.log("ice!!");
-  //   io.to(sender).emit("icezz", ice);
-  //   // socket.to(roomid).emit("ice", ice);
-  // });
+  socket.on("ice", (ice, sender) => {
+    console.log("ice!!");
+    io.to(sender).emit("icezz", ice);
+    // socket.to(roomid).emit("ice", ice);
+  });
   // console.log(socket.id, "connection");
-
   socket.on("join_room", (data) => {
     // 방이 기존에 생성되어 있다면
     if (users[data.room]) {
@@ -251,8 +136,4 @@ io.on("connection", (socket) => {
     delete socketRoom[socket.id];
     socket.broadcast.to(users[roomID]).emit("user_exit", { id: socket.id });
   });
-});
-
-server.listen(PORT, () => {
-  console.log(`server running on ${PORT}`);
 });
